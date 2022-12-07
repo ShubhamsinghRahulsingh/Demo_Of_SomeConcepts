38b11aa1f6dae0efeef59e3d2844f3e452b426c9
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-12-07 14:46:28 +0530

    (Shubham)Add: Project Folder Structure Created

---
+++ DemoProblems/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace DemoProblems
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Demo Part of Some Concepts");
+        }
+    }
+}

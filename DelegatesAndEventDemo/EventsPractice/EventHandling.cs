ba4bbee318229aa0eafbf56eecd59f77fc204261
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-12-07 14:58:29 +0530

    (Shubham)Add: Event Handling concept

---
+++ DelegatesAndEventDemo/EventsPractice/EventHandling.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsPractice
+{
+    public delegate void EventHandler(string x);
+
+    public class EventHandling
+    {
+        public class Operation
+        {
+            public event EventHandler xyz;
+            public void Action(string a)
+            {
+                if(xyz != null)
+                {
+                    xyz(a);
+                    Console.WriteLine(a);
+                }
+                else
+                {
+                    Console.WriteLine("Not Registered");
+                }
+            }
+        }
+        public static void CatchEvent(string a)
+        {
+            Console.WriteLine("Method Calling");
+        }
+        public static void ImplementEvent()
+        {
+            Operation o = new Operation();
+            o.Action("Event Calling");
+            o.xyz += new EventHandler(CatchEvent);
+            o.Action("Event Called");
+        }
+    }
+}
---
+++ DelegatesAndEventDemo/EventsPractice/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace EventsPractice
+{
+    public delegate void DelEventHandler();
+    class Program
+    {
+        public static event DelEventHandler add;
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Events Practice Session");
+            add +=new DelEventHandler(India);
+            add += new DelEventHandler(America);
+            add += new DelEventHandler(England);
+            add();
+            Console.WriteLine("***********EventHandling**************");
+            EventHandling.ImplementEvent();
+        }
+        public static void India()
+        {
+            Console.WriteLine("Hello India");
+        }
+        public static void America()
+        {
+            Console.WriteLine("Hello America");
+        }
+        public static void England()
+        {
+            Console.WriteLine("Hello England");
+        }
+    }
+}
